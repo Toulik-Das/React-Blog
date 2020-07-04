8c5951bc16d953683c10e3e4bfd4e387e3b4214b
Toulik Das <dastoulik12@gmail.com>
2020-07-04 08:09:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,35 +3,48 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 //Create React Component
 const App = function () {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Rose"
-        timeAgo="Today at 6:00 PM"
-        comment="It's a fantastic blog"
-        dp={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Saij"
-        timeAgo="Today at 10:00 PM"
-        comment="Oh! wow"
-        dp={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Toulik"
-        timeAgo="Today at 7:00 PM"
-        comment="loved it!"
-        dp={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Tulip"
-        timeAgo="Today at 6:30 PM"
-        comment="Upload some cool stuffs!"
-        dp={faker.image.avatar()}
-      />
+      <ApprovalCard>
+         <CommentDetail
+            author="Rose"
+            timeAgo="Today at 6:00 PM"
+            comment="It's a fantastic blog"
+            dp={faker.image.avatar()}
+          />
+      </ApprovalCard>
+
+     <ApprovalCard>
+        <CommentDetail
+          author="Saij"
+          timeAgo="Today at 10:00 PM"
+          comment="Oh! wow"
+          dp={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+           author="Toulik"
+           timeAgo="Today at 7:00 PM"
+           comment="loved it!"
+           dp={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+         <CommentDetail
+           author="Jeny"
+           timeAgo="Today at 6:30 PM"
+           comment="Upload some cool stuffs!"
+           dp={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
     </div>
   );
 };
